3100bba88f5b0c56227feccf03ab2463b833c193
munafio <munafaqeelmahdi@gmail.com>
2021-06-04 18:14:39 +0300

    Dashboard message modified.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    You're logged in!<br />
+                    <a href="/{{ config('chatify.routes.prefix') }}">Click here</a> 👈 , to navigate to Chatiy messenger.
                 </div>
             </div>
         </div>
