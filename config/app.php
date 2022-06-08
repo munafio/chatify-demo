3232e861621019583670b95147c64454164b1820
munafio <munafaqeelmahdi@gmail.com>
2022-06-08 15:39:18 +0300

    google drive filesystem removed

--- config/app.php
+++ config/app.php
@@ -174,7 +174,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\GoogleDriveServiceProvider::class,
 
     ],
 
